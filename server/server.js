1c76297423248b6c47f95ee17b21cd5720cee63d
Jon Harvey <joncodes@proton.me>
2024-09-30 12:21:56 -0300

    CORS fix

--- server/server.js
+++ server/server.js
@@ -1,30 +1,44 @@
-const express = require('express')
-const connectDB = require('./config/db')
-const cors = require('cors')
-const bodyParser = require('body-parser')
+const express = require('express');
+const connectDB = require('./config/db');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 3000
-
-const app = express()
+const app = express();
 
 // connect to mongodb
-connectDB()
-  .then(() => {
-    app.listen(PORT, console.log(`Server running on port ${PORT}`))
-  })
-
-  app.use(cors({
+connectDB().then(() => {
+  app.listen(PORT, console.log(`Server running on port ${PORT}`));
+});
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://soundslip.netlify.app")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  )
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "POST, PUT, PATCH, GET, DELETE"
+    )
+    return res.status(200).json({})
+  }
+  next()
+})
+app.use(
+  cors({
     origin: process.env.FRONTEND_URL || '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-  }))
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
-app.use(bodyParser())
+  })
+);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(bodyParser());
 
 // Routes
 
-app.use('/', require('./routes/index'))
-app.use('/soundslips', require('./routes/soundslips'))
+app.use('/', require('./routes/index'));
+app.use('/soundslips', require('./routes/soundslips'));
